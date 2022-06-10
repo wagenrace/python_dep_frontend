bcffb9fd639af6d437df9b0b63dc4ec1f434f07d
tomnijhof <tom.nijhof@cytosmart.com>
2022-06-10 12:02:01 +0200

    add basic connection to herokuapp
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { storeToRefs } from "pinia";
+import { usePythonDepsStore } from './stores/pythonDepStore';
+
+const store = usePythonDepsStore();
+const { isLoading,
+  packageNames,
+  licenses,
+  totalSizeBytes
+} = storeToRefs(store);
+const { loadDeps } = store
+loadDeps()
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>
+    isLoading {{ isLoading }}
+  </p>
+  <p>
+    Packages {{ packageNames }}
+  </p>
+  <p>
+    licenses {{ licenses }}
+  </p>
+  <p>
+    totalSizeBytes {{ totalSizeBytes }}
+  </p>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/stores/pythonDepStore.js
@@ -0,0 +1,56 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { ref } from "vue";
+
+export const usePythonDepsStore = defineStore("pythonDepsStore", () => {
+  // State
+  const startPackages = ref(["tomni",
+  "numpy",
+  "scikit-image"]);
+  const packageNames = ref([]);
+  const licenses = ref([]);
+  const totalSizeBytes = ref(-1);
+  const isLoading = ref(false);
+
+  async function _fetchResults() {
+    // Fetching new data
+    const response = await axios
+      .post("https://python-dep-graph.herokuapp.com/getPackagesInfo", startPackages.value)
+      .catch((e) => {
+        const Error =
+          e.response.data.Error || "The experiment could not be found";
+        return { Error };
+      });
+
+    return response;
+  }
+
+  async function loadDeps() {
+    isLoading.value = true;
+
+    const response = await _fetchResults();
+
+    const error = response.Error || null
+    
+    if (error) {
+      isLoading.value = false;
+      return;
+    }
+
+    console.log(response)
+
+    packageNames.value = response.data.packageNames;
+    licenses.value = response.data.licenses;
+    totalSizeBytes.value = response.data.totalSizeBytes;
+
+    isLoading.value = false;
+  }
+
+  return {
+    isLoading,
+    packageNames,
+    licenses,
+    totalSizeBytes,
+    loadDeps,
+  };
+});
