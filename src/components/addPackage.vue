d0c542de4f966e5ab0910ceefe4658d5177dcec1
tomnijhof <tom.nijhof@cytosmart.com>
2022-06-10 22:27:20 +0200

    better place holder
    Signed-off-by: tomnijhof <tom.nijhof@cytosmart.com>

--- src/components/addPackage.vue
+++ src/components/addPackage.vue
@@ -18,7 +18,7 @@ function addPackage(e) {
     <div class='flex justify-center w-full'>
         <form v-on:submit="addPackage" class="md:w-3/4 w-full m-4">
             <div class="flex-col space-y-4">
-                <input type="text" placeholder="Name" v-model="newPackageName"
+                <input type="text" placeholder="e.q. tensorflow, numpy, tomni, pandas" v-model="newPackageName"
                     class="border-2 w-3/4 px-4 py-2 rounded-md text-md text-gray-700 outline-none" />
                 <button
                     class="w-1/4 mt-4 bg-gradient-to-tr from-blue-700 to-blue-900 text-white py-2 rounded-md text-lg font-semibold">
